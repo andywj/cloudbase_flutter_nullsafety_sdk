3d031a88c926d702ec51117919a7a4e7f9f1eb4d
wangjing <414271986@qq.com>
2022-03-27 20:58:26 +0800

    ✨ feat(function): 新增调用方式
    新增无鉴权云函数调用

--- packages/cloudbase_function_nullsafety/lib/cloudbase_function_nullsafety.dart
+++ packages/cloudbase_function_nullsafety/lib/cloudbase_function_nullsafety.dart
@@ -29,11 +29,42 @@ class CloudBaseFunction {
     CloudBaseRequest cloudbaseRequest = CloudBaseRequest(_core);
     CloudBaseResponse res = await cloudbaseRequest.post(_action, callParams);
 
-    // if (res == null) {
-    //   throw new CloudBaseException(
-    //       code: CloudBaseExceptionCode.NULL_RESPONES,
-    //       message: "unknown error, res is null");
-    // }
+    // 存在 code，说明返回值存在异常
+    if (res.code != null) {
+      throw new CloudBaseException(code: res.code!, message: res.message ?? '');
+    }
+
+    // 尝试解析响应值
+    String responseData = res.data['response_data'];
+
+    try {
+      dynamic data = jsonDecode(responseData);
+      res.data = data;
+      return res;
+    } catch (e) {
+      // 解析失败，返回原值
+      return res;
+    }
+  }
+
+  Future<CloudBaseResponse> callFunctionWithoutAuth(String name,
+      [Map<String, dynamic>? params]) async {
+    if (name.isEmpty) {
+      throw new CloudBaseException(
+          code: CloudBaseExceptionCode.EMPTY_PARAM,
+          message: 'function name must not be empty');
+    }
+
+    Map<String, dynamic> callParams = {'function_name': name};
+
+    if (params != null) {
+      callParams['request_data'] = jsonEncode(params);
+    }
+
+    CloudBaseRequest cloudbaseRequest = CloudBaseRequest(_core);
+    CloudBaseResponse res =
+        await cloudbaseRequest.postWithoutAuth(_action, callParams);
+    res = CloudBaseResponse.fromMap(res.data);
 
     // 存在 code，说明返回值存在异常
     if (res.code != null) {
